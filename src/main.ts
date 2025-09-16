38c454dd1f4b01f9f9cbedbec5deccd2309b2e5b
Hyago <hyago@MacBook-Air-de-Hyago.local>
2025-09-16 14:46:13 -0300

    version 2.5

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   }));
   
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'https://nerdz.otimizeagenda.com',
     credentials: true,
   });
 
